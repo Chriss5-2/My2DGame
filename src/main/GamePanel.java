ed621d85d045b5f6f29af9a9d69ef1063ee82c34
Christian Luna <christiangiovannixd@gmail.com>
2024-11-28 15:04:48 -0500

    Asignacion de teclas

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,12 +15,20 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; //768
     final int screenHeight = tileSize * maxScreenRow;//576
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //Posiciones
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -31,5 +39,42 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        while(gameThread != null){
+
+            //UPDATE THE COORDINATES
+            update();
+
+            //DRAW THE NEW COORDINATES IN THE SCREEN
+            repaint();
+            System.out.println("Running");
+        }
+    }
+
+    public void update(){
+        if(keyH.upPressed==true){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed==true){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed==true){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed==true){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.white);
+
+        g2d.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2d.dispose();
     }
 }
